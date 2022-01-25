d4075c05be67148cc7e9ea5e8e1e7f770b9eaa23
Garrett Dechance <garrett_dechance@my.cuesta.edu>
2022-01-24 16:09:23 -0800

    [Assignment 0-1] Intro activity

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+    //add another line here
+    //and another one to show the changes committed
+}
