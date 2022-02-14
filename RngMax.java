3dddbbde702c510095bb5032f3af78579b56b1d7
Garrett Dechance <garrett_dechance@my.cuesta.edu>
2022-02-13 20:35:16 -0800

    [Assignment 3-2] Greatest Random Number

---
+++ RngMax.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+public class RngMax {
+    public static void main(String [] args){
+        Random randGen = new Random();
+        int x = randGen.nextInt(10);
+        int y = randGen.nextInt(10);
+        int z = randGen.nextInt(10); //Variables block
+        
+        System.out.println("Generated Random Numbers: " + x + ", " + y + ", " + z);
+        if(x > y && x > z){
+            System.out.println("Greatest number: " + x);
+        }
+        else if(y > x && y > z){
+            System.out.println("Greatest number: " + y);
+        }
+        else if(z > y && z > x){
+            System.out.println("Greatest number: " + z);
+        }
+    }
+}
